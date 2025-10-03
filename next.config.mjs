c83b656b47ba7cfe82fffb516726b067c6703d45
jrittenhouse <jrittenhouse56@gmail.com>
2025-10-03 16:24:48 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
+const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
+
 /** @type {import('next').NextConfig} */
-import createMDX from '@next/mdx'
-
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-})
-
-const nextConfig = withMDX({
+module.exports = withMDX({
   pageExtensions: ['ts', 'tsx', 'mdx'],
   reactStrictMode: true,
-  images: { remotePatterns: [] },
+  images: { remotePatterns: [] }
 })
 
-export default nextConfig;
