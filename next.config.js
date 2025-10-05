d2a863b8afdbea54adf3c005e982531fb56392eb
jrittenhouse <jrittenhouse56@gmail.com>
2025-10-05 15:38:44 -0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
-
-/** @type {import('next').NextConfig} */
-module.exports = withMDX({
-  pageExtensions: ['ts', 'tsx', 'mdx'],
-  reactStrictMode: true,
-  images: { remotePatterns: [] }
-})
