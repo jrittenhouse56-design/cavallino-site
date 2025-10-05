71163dd222307fc38f6753d91b247702e34281cd
jrittenhouse <jrittenhouse56@gmail.com>
2025-10-05 15:36:45 -0700

    Delete Card.tsx

--- Card.tsx
+++
@@ -1,2 +0,0 @@
-import { ReactNode } from 'react';
-export default function Card({title,children}:{title:string;children:ReactNode}){ return (<div className="card"><h3 style={{marginTop:0}}>{title}</h3><div>{children}</div></div>); }
